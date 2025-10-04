6b8a1a4dce5e005f2fc022deee2960880a96e554
unknown <jfranciaminaya@gmail.com>
2025-10-03 19:08:55 -0500

    Registro completo

--- app/Views/confirmacion.php
+++ app/Views/confirmacion.php
@@ -3,10 +3,18 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Encuestas</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </head>
 <body>
-  <h1>Registrado correctamente</h1>
-  <a href="<?= base_url() ?>">Nueva encuesta</a>
+  <div class="container mt-2">
+    <div class="text-center">
+      <h2>Registrado correctamente</h2>
+      <a href="<?= base_url() ?>">Nueva encuesta</a>
+    </div>
+  </div>
 </body>
 </html>
